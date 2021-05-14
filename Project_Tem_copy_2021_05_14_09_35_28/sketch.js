c215fc92ae051b3bd5509536686793c7be49369f
Rehaan112008 <84014497+Rehaan112008@users.noreply.github.com>
2021-05-14 15:24:30 +0530

    Add files via upload

---
+++ Project_Tem_copy_2021_05_14_09_35_28/sketch.js
@@ -0,0 +1,14 @@
+var player,playerimg,path,pathimg
+function preload(){
+  playerimg=loadIma
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+}
+
+function draw() {
+  background(0);
+
+}
